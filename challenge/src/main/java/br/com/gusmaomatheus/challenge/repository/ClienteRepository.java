39faa410ba5211a9f76add8bb19d86aa5f01c3cd
Matheus <mgusmao.dev@gmail.com>
2025-06-27 01:44:27 -0300

    feat(repository): create method to search for 'Cliente' by name

--- challenge/src/main/java/br/com/gusmaomatheus/challenge/repository/ClienteRepository.java
+++ challenge/src/main/java/br/com/gusmaomatheus/challenge/repository/ClienteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.gusmaomatheus.challenge.model.entity.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
+    boolean existsByNome(String nome);
 }
