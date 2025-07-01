acab342eba13a72050b1d752cd72a0ff62d8eabc
Matheus <mgusmao.dev@gmail.com>
2025-06-30 21:38:28 -0300

    feat(controller): validate body in POST /api/v1/clientes

--- challenge/src/main/java/br/com/gusmaomatheus/challenge/controller/ClienteController.java
+++ challenge/src/main/java/br/com/gusmaomatheus/challenge/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.gusmaomatheus.challenge.controller;
 import br.com.gusmaomatheus.challenge.model.dto.ClienteRequest;
 import br.com.gusmaomatheus.challenge.model.entity.Cliente;
 import br.com.gusmaomatheus.challenge.service.ClienteService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +20,7 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseEntity inserir(@RequestBody ClienteRequest clienteRequest) {
+    public ResponseEntity inserir(@Valid @RequestBody ClienteRequest clienteRequest) {
         service.inserir(new Cliente(clienteRequest.nome()));
 
         return ResponseEntity.ok().build();
